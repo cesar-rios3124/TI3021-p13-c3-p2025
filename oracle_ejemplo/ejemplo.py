0e002c4f6a6a9dff3b154b129c956c25f93d814b
cesar-rios3124 <cesitar.com31@gmail.com>
2025-11-07 12:13:13 -0300

    ejemplo 07-11-2025

---
+++ oracle_ejemplo/ejemplo.py
@@ -0,0 +1,31 @@
+import oracledb
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+username = os.getenv("ORACLE_USER")
+dsn = os.getenv("ORACLE_DSN") 
+password = os.getenv("ORACLE_PASSWORD")
+
+def get_connection():
+    return oracledb.connect(user=username, password=password, dsn=dsn)
+
+def create_table_persona():
+    query = (
+    "CREATE TABLE personas ("
+    "rut VARCHAR2(50) PRIMARY KEY,"
+    "nombres VARCHAR2(200),"
+    "apellidos VARCHAR2(200),"
+    "fecha_nacimiento DATE,"
+    "cod_area VARCHAR2(20),"
+    "numero_telefono VARCHAR2(50)"
+    ")"
+)
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cursos:
+                cursos.execute(query)
+                print("Tabla 'personas' creada.")
+    except oracledb.DatabaseError as error:
+        print(f"No se pudo crear la tabla: {error}")
+
